62a091004a83a5491facc8bba49be55d1ee3dcce
purav7838 <44437936+purav7838@users.noreply.github.com>
2020-04-13 23:36:36 +0530

    Add files via upload

---
+++ src/main/java/com/data/bridge/connection/ConnectionProvider.java
@@ -0,0 +1,17 @@
+package com.data.bridge.connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionProvider {
+	private static Connection c;
+	public static Connection getConnection() throws ClassNotFoundException, SQLException
+	{
+		
+		Class.forName("org.postgresql.Driver");
+		c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/data_bridge", "postgres", "purav");
+		c.setAutoCommit(false);
+		return c;
+	}
+}
